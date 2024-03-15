e885657b03313960ed4f67693d9e0f77c74eed40
evinsdesign <kelvinsaputra024@gmail.com>
2024-03-15 11:11:29 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import FirstPage from './views/FirstPage.vue'
+import SecondPage from './views/SecondPage.vue'
+</script>
+
+<template>
+  <RouterLink :to="{ name: 'SecondPage' }"><SecondPage /></RouterLink>
+</template>
+
+<style scoped></style>
---
+++ src/components/Buttons.vue
@@ -0,0 +1,16 @@
+<script>
+import OpenIcon from '@/components/icons/OpenIcon.vue'
+
+export default {
+  components: {
+    OpenIcon
+  }
+}
+</script>
+<template>
+  <button
+    class="bg-primary rounded-[30px] flex items-center px-5 py-1 text-white text-poppins mt-2 font-medium xxsm:text-sm xsm:text-md sm:text-lg md:text-xl hover:scale-105 hover:transition-all"
+  >
+    <OpenIcon /> Buka Undangan
+  </button>
+</template>
